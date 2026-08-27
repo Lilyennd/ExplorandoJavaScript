390240f5b8b9b6af9af3e34a09a364c5331564d9
Lilyennd <victoriariffo21@gail.com>
2026-08-27 13:04:32 -0400

    ola1

---
+++ funciones.js
@@ -0,0 +1,21 @@
+function validarDatos(){
+    document.addEventListener
+
+    let nombre = document.getElementById("nombre").value
+
+    let correo = document.getElementById("correo").value
+
+    if (nombre == "") {
+        alert("error campo vacio");
+        return false;
+    }
+
+    
+    if (correo === "") {
+        alert("error campo vacio");
+        return false; 
+    }
+
+    alert("¡Todo bien! Enviando formulario...");
+    return true;
+}
